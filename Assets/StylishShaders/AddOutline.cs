08d948be0133a9a5f5c4d80fa331b07de3e35aec
Mylo <36931363+gitmylo@users.noreply.github.com>
2022-02-19 15:07:01 +0100

    Auto recursive outline add script

---
+++ Assets/StylishShaders/AddOutline.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class AddOutline : MonoBehaviour
+{
+    public Material outlineMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Apply the outline
+        addOutline(this.gameObject);
+    }
+
+    void addOutline(GameObject g)
+    {
+        var renderers = g.GetComponentsInChildren<Renderer>();
+        foreach (var renderer in renderers)
+        {
+            var go = renderer.gameObject;
+            var c = Instantiate(go);
+            var r = c.GetComponent<Renderer>();
+            
+            //outlines don't cast shadows dummy
+            r.shadowCastingMode = ShadowCastingMode.Off;
+            
+            //set material/materials of the clone to the outline material
+            if (r.materials != null && r.materials.Length != 0)
+                for (int i = 0; i < r.materials.Length; i++) r.materials[i] = outlineMaterial;
+            else if (r.material != null) r.material = outlineMaterial;
+            
+        }
+    }
+}
